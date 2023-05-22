4045a63e36e00c48839aa8afd669101ee5b98144
MoulaDilshan <skmoulaseo@gmail.com>
2023-05-22 17:37:42 +0530

    all

---
+++ src/main/java/buswar/MicroProfileConfiguration.java
@@ -0,0 +1,16 @@
+
+package buswar;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+
+/**
+ * @author rhuanrocha
+ */
+
+
+@ApplicationPath("/")
+public class MicroProfileConfiguration extends Application {
+
+}
---
+++ src/test/java/buswar/endpoint/MyEndpointTest.java
@@ -0,0 +1,8 @@
+package buswar.resource;
+
+
+public class MyEndpointTest {
+
+
+
+}
